3776c48c0f649fe541465878bd2e082865d85149
MoyankGiri <56188720+MoyankGiri@users.noreply.github.com>
2022-04-19 14:01:15 +0530

    added separate databases for each microservice

--- kubectlStop.sh
+++ kubectlStop.sh
@@ -3,3 +3,5 @@ kubectl scale deploy postmicroservice --replicas=0
 kubectl scale deploy authmicroservice --replicas=0
 kubectl scale deploy apigateway --replicas=0
 kubectl scale deploy mongodb --replicas=0
+
+# kubectl scale deploy -n default --replicas=0 --all # if using different namespace use that instead of default
