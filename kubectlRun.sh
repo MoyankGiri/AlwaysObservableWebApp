79fb65437e8102f916c216ed63b552d294b12c06
chandradharrao <chandradhar.rao@gmail.com>
2022-06-24 23:35:43 +0530

    fixed kubernetes app.yaml file hence fixing entire webapp

--- kubectlRun.sh
+++ kubectlRun.sh
@@ -1,1 +1,9 @@
-kubectl apply -f app.yaml
+kubectl apply -f $(pwd)/app/Deployment.yml
+
+for msvc in auth_svc post_svc comments_svc
+do
+    kubectl apply -f $(pwd)/microservices/$msvc/src/Deployment.yml
+    kubectl apply -f $(pwd)/microservices/$msvc/src/MongoDeployment.yml
+done
+
+kubectl port-forward svc/apigateway 5000  
