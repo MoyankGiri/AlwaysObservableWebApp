073946f4bea70fcd02d1a32972b0fb9eacbedfe5
MoyankGiri <56188720+MoyankGiri@users.noreply.github.com>
2022-06-28 23:22:45 +0530

    Added functions to workload script for post and comments

---
+++ workload_check.py
@@ -0,0 +1,4 @@
+import workload_simulator
+
+#workload_simulator.createBlog_variedLengths('http://localhost:5000/createBlog',0,MAXPOSTS = 5)
+#workload_simulator.addCommentsToAllBlogs()
--- workload_simulator.py
+++ workload_simulator.py
@@ -6,6 +6,7 @@ from turtle import title
 import requests
 import threading
 from multiprocessing import Process
+import re
 
 MAX = 100
 TIME = 3
@@ -52,6 +53,47 @@ def create_blog(url,token,count):
         setInterval(TIME,create_blog(url,token,count+1))
     else:
         return
+
+# for varied length of posts and checking time taken to fetch
+# can use for response size check and latency check
+def createBlog_variedLengths(url,count,length = 500,MAXPOSTS = 10):
+
+    def getRandomString(lengthOfString,s = f'This Blog Has length = {length} This Blog Has length = {length} This Blog Has length = {length}\n'):
+        return s * (lengthOfString//len(s))
+
+    res = requests.post(url = "http://localhost:5000/login",data = {'username':'admin','password':'admin'})
+    cookie = {'token':res.cookies.get_dict()['token']}
+
+    res = requests.post(url,data={'title':f'Blog {count}','author':'random_author','body':f"{getRandomString(length)}"},cookies=cookie)
+    print("")
+
+    if count < MAXPOSTS:
+        setInterval(TIME,createBlog_variedLengths(url,count+1,length * 2,MAXPOSTS))
+    else:
+        return
+
+def getAllBlogs():
+    res = requests.get(url='http://localhost:5000/readBlogs',params=None)
+    allBlogID = [str(re.findall('id=.*? ',i)[0].split('=')[1][:-1].replace('"','')) for i in [x.group() for x in re.finditer('<button.*?>(.+?)</button>',res.text)]]
+    return list(set(allBlogID))
+
+def addCommentsToAllBlogs():
+    res = requests.post(url = "http://localhost:5000/login",data = {'username':'admin','password':'admin'})
+    cookie = {'token':res.cookies.get_dict()['token']}
+    #print(res.cookies.get_dict()['token'])
+    allBlogs = getAllBlogs()
+    for blogID in allBlogs:
+        postToCreateComment(blogID,cookie=cookie,MAX_COUNT=randint(5,20))
+
+def postToCreateComment(blogID,url = 'localhost:5000/createComment',MAX_COUNT = 5,cookie = None):
+    count = 0
+    if cookie is None:
+        res = requests.post(url = "http://localhost:5000/login",data = {'username':'admin','password':'admin'})
+        cookie = {'token':res.cookies.get_dict()['token']}
+    while count < MAX_COUNT:
+        requests.post(url = f"http://{url}?blogID={blogID}",data = {"title":f"Comment title_{count}_{blogID}","body":f"Comment body_{count}_{blogID}\n"*randint(5,100),"author":f"Comment author_{count}_{blogID}","blogID":blogID},cookies=cookie)
+        count += 1
+
 
 randomization_config={'urls':['http://localhost:5000/login','http://localhost:5000/home','http://localhost:5000/readBlogs',],'bool':True}
 
